c9ea751a4178357829c04829762281f7585d9a7f
deemson <deemson.k@gmail.com>
2013-07-25 16:18:50 +0300

    web app and jetty

--- java/graguijer/src/main/java/graguijer/Main.java
+++ java/graguijer/src/main/java/graguijer/Main.java
@@ -1,7 +1,22 @@
 package graguijer;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws Exception {
+
+        WebAppContext webAppContext = new WebAppContext("web", "/");
+
+        Server server = new Server(8080);
+        server.setHandler(webAppContext);
+        server.start();
+        server.join();
+        System.out.println("asd");
     }
 }

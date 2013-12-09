c11df9905b084141ed618461d28023d815077036
deemson <deemson.k@gmail.com>
2013-12-09 17:03:15 +0200

    dozer example fixes

--- java/dozer/src/main/java/example1/Example1.java
+++ java/dozer/src/main/java/example1/Example1.java
@@ -3,34 +3,24 @@ package example1;
 import example1.objects.Object1;
 import example1.objects.Object2;
 import org.dozer.DozerBeanMapper;
-import org.dozer.loader.api.BeanMappingBuilder;
 
-/**
- * @author dmitriy.kurkin
- */
 public class Example1 {
 
     public static void main(String[] args) {
 
         DozerBeanMapper mapper = new DozerBeanMapper();
-        mapper.addMapping(new BeanMappingBuilder() {
-            @Override
-            protected void configure() {
-                mapping(Object1.class, Object2.class);
-            }
-        });
 
         Object1 object1 = new Object1("some", "some_other", 42);
-        System.out.println("Initial object: ["+object1+"]");
+        System.out.println("\tInitial object: ["+object1+"]");
 
         Object2 object2 = mapper.map(object1, Object2.class);
-        System.out.println("Mapped object: ["+object2+"]");
+        System.out.println("\tMapped object: ["+object2+"]");
 
         object2.setInteger(43);
-        System.out.println("Changed object: ["+object2+"]");
+        System.out.println("\tChanged object: ["+object2+"]");
 
         mapper.map(object2, object1);
-        System.out.println("Mapped back object: ["+object1+"]");
+        System.out.println("\tMapped back object: ["+object1+"]");
 
     }
 
